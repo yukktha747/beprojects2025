3df8750abfbfdca952fdf406ec6844e5927f14e1
Dheeraj <dhiraajroy123@gmail.com>
2024-10-20 00:14:44 +0530

    added delete file and folder option

--- filemanage/urls.py
+++ filemanage/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import browse_folder
+from .views import browse_folder, delete_item
 
 app_name = "filemanage"
 
 urlpatterns = [
     path('', browse_folder, {'folder_path': ''}, name='home'),
+    path('delete/<path:item_path>/', delete_item, name='delete_item'),
     path('<path:folder_path>', browse_folder, name='browse_folder'),
 ]
