62c23b36912896f72642a29a4237dc771a66fdc3
Anirudh <aayanamanirudh@gmail.com>
2024-10-20 13:14:48 +0530

    fixed bugs for view

--- filemanage/urls.py
+++ filemanage/urls.py
@@ -4,6 +4,7 @@ from .views import browse_folder, view_file,delete_item
 app_name = "filemanage"
 
 urlpatterns = [
+    path('view/<str:file_name>/', view_file, name='view_file_no_folder'),
     path('view/<path:folder_path>/<str:file_name>/', view_file, name='view_file'),
     path('', browse_folder, {'folder_path': ''}, name='home'),
     path('delete/<path:item_path>/', delete_item, name='delete_item'),
