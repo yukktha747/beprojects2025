478ec6ca824b73a06c13962cc3eaf8b670dc3b35
Anirudh <aayanamanirudh@gmail.com>
2024-10-29 15:37:10 +0530

    Start fresh for v2

--- authenticate/views.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.views import LoginView
-from django.urls import reverse_lazy
-from .forms import CustomAuthenticationForm
-
-class CustomLoginView(LoginView):
-    template_name = 'login.html'
-    form_class = CustomAuthenticationForm
-    redirect_authenticated_user = True
-    next_page = '/'
