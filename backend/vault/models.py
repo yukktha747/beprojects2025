1c4a376724daf9ccfce210051d6194aaea072e6c
Anirudh <aayanamanirudh@gmail.com>
2024-11-03 21:30:02 +0530

    reworked fav and completed basic API Routes

--- backend/vault/models.py
+++ backend/vault/models.py
@@ -4,9 +4,16 @@ from django.contrib.auth.models import User
 class UserImage(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     image_url = models.URLField()  # URL to access the image
-    is_favorite = models.BooleanField(default=False)
+    # is_favorite = models.BooleanField(default=False)
     is_in_trash = models.BooleanField(default=False)
     privacy = models.CharField(max_length=10, choices=[('public', 'Public'), ('private', 'Private')])
 
     def __str__(self):
         return f"{self.user.username}  {self.id}"
+
+class Favourite(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(UserImage, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.image }"
--- backend/vault/urls.py
+++ backend/vault/urls.py
@@ -1,12 +1,28 @@
 from django.urls import path
-from .views import *
+from .views import (
+    upload_images,
+    get_all_public_photos,
+    add_to_favorites,
+    get_user_favorites,
+    remove_from_favorites,
+    is_favorite,
+    get_user_trash,
+    mark_image_as_trash,
+    restore_from_trash,
+    get_user_private_images
+)
 
 urlpatterns = [
-    path('get_fav/',get_user_favorites,name='get_user_favorites'),
-    path('get_trash/',get_user_trash,name='get_user_trash'),
-    path('upload_images/',upload_images,name='upload_images'),
-    path('mark_image_as_trash/',mark_image_as_trash,name='mark_image_as_trash'),
-    path('add_to_favorites/',add_to_favorites,name='add_to_favorites'),
-    path('get_all/',get_all_public_photos,name='get_all_public_photos'),
-
+    path('upload_images/', upload_images, name='upload_images'),
+    path('get_all_public_photos/', get_all_public_photos, name='get_all_public_photos'),
+    path('get_user_private_images/', get_user_private_images, name='get_user_private_images'),
+    # Favorites related
+    path('add_to_favorites/', add_to_favorites, name='add_to_favorites'),
+    path('get_user_favorites/', get_user_favorites, name='get_user_favorites'),
+    path('remove_from_favorites/', remove_from_favorites, name='remove_from_favorites'),
+    path('is_favorite/<int:image_id>/', is_favorite, name='is_favorite'),
+    # Trash related
+    path('get_user_trash/', get_user_trash, name='get_user_trash'),
+    path('mark_image_as_trash/', mark_image_as_trash, name='mark_image_as_trash'),
+    path('restore_from_trash/', restore_from_trash, name='restore_from_trash'),
 ]
