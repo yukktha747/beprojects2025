203a95f872a79646905161432d3d9a5920712b7b
Anirudh <aayanamanirudh@gmail.com>
2024-12-10 21:38:40 +0530

    search and tag completed

--- backend/vault/admin.py
+++ backend/vault/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import UserImage,Favourite
+from .models import UserImage,Favourite, Tag
 # Register your models here.
 admin.site.register(UserImage)
 admin.site.register(Favourite)
+admin.site.register(Tag)
