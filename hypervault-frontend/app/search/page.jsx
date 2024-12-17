809f42e0618a13dc953b69b7348a6229d966ccac
Dheeraj <dhiraajroy123@gmail.com>
2024-12-17 23:40:45 +0530

    fixed ssr

--- hypervault-frontend/app/search/page.jsx
+++ hypervault-frontend/app/search/page.jsx
@@ -1,44 +1,9 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import useAuthStore from "@/stores/useAuthStore";
-import { isLoggedIn as ili, search } from "@/calls";
-import { useState, useEffect } from "react";
 import dynamic from "next/dynamic";
 
-const ListSection = dynamic(() => import("@/components/ListSection"), {
-  ssr: false,
-});
+const SearchContent = dynamic(() => import("@/components/SearchContent"), { ssr: false });
 
 export default function Search() {
-  const { isLoggedIn, setIsLoggedIn } = useAuthStore();
-  const [files, setFiles] = useState([]);
-  const router = useRouter();
-  const searchParams = useSearchParams();
-
-  const searchQuery = searchParams.get("query");
-  const documentType = searchParams.get("type");
-
-  useEffect(() => {
-    if (ili()) setIsLoggedIn(true);
-    else setIsLoggedIn(false);
-    if (!isLoggedIn) router.push("/auth/login");
-  }, [isLoggedIn]);
-
-  async function getData() {
-    if (searchQuery) {
-      const results = await search(searchQuery, documentType);
-      setFiles(results);
-    }
-  }
-
-  useEffect(() => {
-    getData();
-  }, [searchQuery, documentType]);
-
-  return (
-    <div className="mt-36">
-      <ListSection data={files} refreshData={getData} />
-    </div>
-  );
+  return <SearchContent />;
 }
