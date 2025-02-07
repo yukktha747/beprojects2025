7196f67a2461cff8cfbf91145f7079415480a0b1
yukkthas.com <120043198+yukktha747@users.noreply.github.com>
2025-02-07 14:49:47 +0530

    Add files via upload

---
+++ Team059/main.js
@@ -0,0 +1,106 @@
+$(document).ready(function () {
+
+    eel.init()()
+
+    $('.text').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "bounceIn",
+        },
+        out: {
+            effect: "bounceOut",
+        },
+
+    });
+
+    // Siri configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: "1",
+        speed: "0.30",
+        autostart: true
+      });
+
+    // Siri message animation
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in: {
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out: {
+            effect: "fadeOutUp",
+            sync: true,
+        },
+
+    });
+
+    // mic button click event
+
+    $("#MicBtn").click(function () { 
+        eel.playAssistantSound()
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        eel.allCommands()()
+    });
+
+
+    function doc_keyUp(e) {
+        // this would test for whichever key is 40 (down arrow) and the ctrl key at the same time
+
+        if (e.key === 'j' && e.metaKey) {
+            eel.playAssistantSound()
+            $("#Oval").attr("hidden", true);
+            $("#SiriWave").attr("hidden", false);
+            eel.allCommands()()
+        }
+    }
+    document.addEventListener('keyup', doc_keyUp, false);
+
+    // to play assisatnt 
+    function PlayAssistant(message) {
+
+        if (message!= "") {
+
+            $("#Oval").attr("hidden", true);
+            $("#SiriWave").attr("hidden", false);
+            eel.allCommands(message);
+            $("#chatbox").val("")
+            $("#MicBtn").attr('hidden', false);
+            $("#SendBtn").attr('hidden', true);
+
+        }
+
+    }
+   
+    
+
+    // Toggle visibility of Mic and Send buttons
+    function ShowHideButton(message) {
+        if (message.length== 0) {
+            $("#MicBtn").attr('hidden', false);
+            $("#SendBtn").attr('hidden', true);
+        } else {
+            $("#MicBtn").attr('hidden', true);
+            $("#SendBtn").attr('hidden', false);
+        }
+    }
+    
+    // Keyup event handler on the chatbox
+    $("#chatbox").keyup(function () {
+        const message = $("#chatbox").val();
+        ShowHideButton(message);
+    });
+    
+    // Click event handler for the Send button
+    $("#SendBtn").click(function () {
+        const message = $("#chatbox").val();
+        PlayAssistant(message);
+    });
+    
+});
